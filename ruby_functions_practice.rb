116c7e1b6c9f4700bdf5d72f6eb7442793c1d65d
EismanW <pl.amosso@gmail.com>
2019-10-15 15:49:14 +0100

    Added num to month function

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -29,3 +29,32 @@ end
 def add_string_as_number(string_num_1, string_num_2)
   return string_num_1.to_i() + string_num_2.to_i()
 end
+
+def number_to_full_month_name(month_number)
+  case month_number
+  when 1
+    return "January"
+  when 2
+    return "February"
+  when 3
+    return "March"
+  when 4
+    return "April"
+  when 5
+    return "May"
+  when 6
+    return "June"
+  when 7
+    return "July"
+  when 8
+    return "August"
+  when 9
+    return "September"
+  when 10
+    return "October"
+  when 11
+    return "November"
+  when 12
+    return "December"
+  end
+end
